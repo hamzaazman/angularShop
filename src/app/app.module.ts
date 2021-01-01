43e0cb28f4ceca4a79b6550ff47ef06c2c03072f
hamzaazman <hamzaazman6776@gmail.com>
2021-01-01 14:44:11 +0300

    ngModel imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { NavComponent } from './nav/nav.component';
 import { CategoryComponent } from './category/category.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     NavComponent,
-    CategoryComponent
+    CategoryComponent,
+    ProductFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
