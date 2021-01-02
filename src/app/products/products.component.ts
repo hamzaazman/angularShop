c05547410d425fab55a22839d88f4ac963d5eb4f
hamzaazman <hamzaazman6776@gmail.com>
2021-01-02 14:16:31 +0300

    service added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
-declare let alertify:any;
+import {AlertifyService} from '../services/alertify.service';
 
 @Component({
   selector: 'app-products',
@@ -9,7 +9,7 @@ declare let alertify:any;
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertifyService:AlertifyService) { }
   title = "Ürün Listesi"
   filterText  = ""
 
@@ -20,7 +20,7 @@ export class ProductsComponent implements OnInit {
     {name:"Mouse",description:"Lorem ipsum dolor sit amet",price:3600,categoryID:2,id:2,imageURL:"https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
     {name:"Usb Bellek",description:"Lorem ipsum dolor sit amet",price:2500,categoryID:1,id:1,imageURL:"https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
     {name:"Mp3 Çalar",description:"Lorem ipsum dolor sit amet",price:3600,categoryID:2,id:2,imageURL:"https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
-    {name:"Laptop",description:"Lorem ipsum dolor sit amet",price:2500,categoryID:1,id:1,imageURL:"https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
+    {name:"Usb Bellek",description:"Lorem ipsum dolor sit amet",price:2500,categoryID:1,id:1,imageURL:"https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
     {name:"Laptop",description:"Lorem ipsum dolor sit amet",price:2500,categoryID:1,id:1,imageURL:"https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"},
 
   ]
@@ -30,8 +30,7 @@ export class ProductsComponent implements OnInit {
   }
 
   AddToCart(product){
-    alertify.success( product.name +" added")
-    //alert("Ürün eklendi: " + product.name)
+    this.alertifyService.success(product.name + " added")
   }
 
 
